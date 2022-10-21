5de666cc77dc9c57e4bf0f19b0f9c736f48e07fb
sesilia <1951850741@qq.com>
2022-10-21 23:00:46 +0800

    add the main codes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "area.h"
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    cout<<"Area of point is"<<area()<<'\n';
+    cout<<"Area of square is"<<area(1,1)<<'\n';
+    cout<<"Area of circle is"<<area(0.5)<<'\n';
+    cout<<"Area of trapezium is"<<area(1,0.5,1)<<'\n';
+    cout<<"Area of triangle is"<<area(1,sqrt(1+0.5*0.5),sqrt(1+0.5*0.5),0)<<'\n';
     return 0;
 }
 
